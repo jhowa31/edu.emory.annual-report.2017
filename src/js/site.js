d6611b33629e8e73b9638220f6ff9bab7e9a1fac
jhowa31 <32077853+jhowa31@users.noreply.github.com>
2017-09-25 10:46:42 -0400

    animate css and jquery viewport

--- src/js/site.js
+++ src/js/site.js
@@ -1,1 +1,9 @@
 // Plain Javascript 
+
+// ANIMATE CSS AND JQUERY VIEWPORT
+jQuery(document).ready(function() {
+    jQuery('.fade-in-box').addClass("hide-me").viewportChecker({
+        classToAdd: 'show-me animated fadeIn',
+        offset: 100
+       });
+});
